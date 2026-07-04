3ef1e6d421cc833c3b96cc1d5393a33106c13045
hoa dang cap <hoangvuhoa18@gmail.com>
2026-07-04 17:35:22 +0700

    Cấu hình SSL cho database

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,18 @@
-// File: src/config/database.js
+const { Pool } = require("pg");
 
-// Import thư viện 'pg' mà chúng ta vừa cài đặt
-const { Pool } = require('pg');
-
-// Khởi tạo một "Hồ bơi" (Pool) chứa các kết nối tới PostgreSQL
 const pool = new Pool({
-  user: 'postgres',        // Tên đăng nhập mặc định của PostgreSQL
-  host: 'localhost',       // Địa chỉ máy chủ (đang chạy trên chính máy cá nhân của bạn)
-  database: 'taphoa_db',   // Tên cơ sở dữ liệu (chúng ta sẽ tạo sau)
-  password: 'mật_khẩu',    // Thay bằng mật khẩu bạn đã đặt lúc cài PostgreSQL
-  port: 5432,              // Cổng giao tiếp mặc định của PostgreSQL
+  connectionString: process.env.DATABASE_URL,
+  // Thêm đoạn cấu hình ssl dưới đây để chạy được trên Render
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// Chạy thử kết nối để xem có thành công hay không
-pool.connect()
-  .then(() => console.log('🎉 Tuyệt vời! Đã kết nối thành công với PostgreSQL!'))
-  .catch((err) => console.error('❌ Lỗi kết nối cơ sở dữ liệu:', err.message));
+pool
+  .connect()
+  .then(() =>
+    console.log("🎉 Đã kết nối thành công với PostgreSQL trên Supabase!")
+  )
+  .catch((err) => console.error("❌ Lỗi kết nối cơ sở dữ liệu: ", err.message));
 
-// Xuất cấu hình này ra để các file khác (ví dụ: phần xử lý hàng hóa) có thể sử dụng
 module.exports = pool;
